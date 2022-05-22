38f61096071e3033343daf41b35d151061ca82ad
heggland <kjetilheggland@gmail.com>
2022-05-22 19:30:39 +0200

    feat: unity basics

---
+++ unity/project-assets/unity-basics/Assets/Script/playerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float speed = 5f;
+    Rigidbody myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        myRigidbody.AddForce(movement * speed);
+    }
+}
