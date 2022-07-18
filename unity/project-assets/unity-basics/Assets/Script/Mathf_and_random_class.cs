9faa0e478bd29da6dd833b40a29e1211ec07f451
heggland <kjetilheggland@gmail.com>
2022-07-18 18:37:16 +0200

    feat: unity  basics

---
+++ unity/project-assets/unity-basics/Assets/Script/Mathf_and_random_class.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mathf_and_random_class : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        float myRandomNumber = Random.Range(0f, 15f);
+        Debug.Log(myRandomNumber);
+
+        // Mathf.
+
+    }
+ 
+}
---
+++ unity/project-assets/unity-basics/Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start method was called");
+        // init program
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // keep the update lightweight
+        // check what value player has every 1min or so
+    }
+}
